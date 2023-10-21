1a2de2260604b66161481a0d0df21ab89b1b98de
Sagar <42873729+sagarkrp@users.noreply.github.com>
2023-10-21 17:58:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 
 print("Hello World")
-# print(os.environ.get('first_name'))
-# print(os.environ.get('last_name'))
-# print((os.environ.get('first_name'))=='Sagar')
-# print((os.environ.get('last_name'))=='p')
-# print((os.environ.get('$github.run_number')))
+print(os.environ.get('first_name'))
+print(os.environ.get('last_name'))
+print((os.environ.get('first_name'))=='Sagar')
+print((os.environ.get('last_name'))=='p')
+print((os.environ.get('$github.run_number')))
